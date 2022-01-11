c7ffd8edae756fe33abe425e17862475aac009c7
Scott Park <scott.park.dev@gmail.com>
2022-01-11 09:49:39 -0800

    Made the rock paper scissors game work.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # rock_paper_scissors
+
 Rock Paper Scissors excercise from the Odin Project
+
+Pseudo code:
+
+Variables:
+Computer_Score
+Computer_Pick
+Player_Score
+Player_Pick
+rps_array = [rock, paper, scissor]
+
+1. Computer Play
+
+from rps_array choose random index and return
+
+or
+
+from range 1 to 3,
+if 1, rock; if 2, paper; if 3, scissors;
+
+Either one should work for the purposes.
+
+2. Play Round
+
+Computer_Pick = Computer_Play()
+Player_Pick = read input from user
+if(all loss scenarios)
+return loss message with Player_Pick and Computer_Pick
+Computer_Score++
+if(all win scenarios)
+return win message with Player_Pick and Computer_Pick
+Player_Score++
+else
+return tie message with Player_Pick
+display current score
+
+3. Game (5 times repeat)
+
+while(Player_Score < 5 and Computer_Score < 5)
+if player_score > computer_score
+display "Player wins"
+else
+display "Computer wins"
--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-
+// ROCK PAPER SCISSORS
 
 let rps = ['rock', 'paper', 'scissors'];
+let player_pick, computer_pick;
+let player_score = 0, computer_score = 0;
+
 
 let computerPlay = () =>{
     random = Math.floor(Math.random() * rps.length);
     return (rps[random]);
 }
 
-let playRound = (player, computer) => {
-    if(player === computer){
-        return ("Draw! You both picked " + player);
-    } else if(player === "rock" && computer === "paper"){
-        return ("You lose! " + computer + " beats " + player);
-    } else if(player === "paper" && computer === "scissors"){
-        return ("You lose! " + computer + " beats " + player);
-    } else if(player === "scissors" && computer === "rock"){
-        return ("You lose! " + computer + " beats " + player);
-    } else if(computer === "rock" && player === "paper"){
-        return ("You win! " + player + " beats " + computer);
-    } else if(computer === "paper" && player === "scissors"){
-        return ("You win! " + player + " beats " + computer);
-    } else if(computer === "scissors" && player === "rock"){
-        return ("You win! " + player + " beats " + computer);
+let playRound = () => {
+    computer_pick = computerPlay();
+    player_pick = prompt("Choose: Rock, Paper, or Scissors").toLowerCase();
+
+    if ((player_pick === "rock" && computer_pick === "paper") || (player_pick === "paper" && computer_pick === "scissors") || (player_pick === "scissors" && computer_pick === "rock")){
+        computer_score++;
+        console.log("You lose the round! " + computer_pick + " beats " + player_pick);
+    } else if ((computer_pick === "rock" && player_pick === "paper") || (computer_pick === "paper" && player_pick === "scissors") || (computer_pick === "scissors" && player_pick === "rock")){
+        player_score++;
+        console.log("You win the round! " + player_pick + " beats " + computer_pick);
+    } else if (player_pick === computer_pick){
+        console.log("Draw! You both picked " + player_pick);
     }
+    console.log("You: " + player_score + " Computer: " + computer_score);
 }
 
 let game = () =>{
-    let score = 0;
-    for(let i = 0; i < 5; i++){
-        let user_input = prompt("Choose: Rock, Paper, or Scissors").toLowerCase();
-        console.log(playRound(user_input, computerPlay()));
-    }
     
+
+    while(player_score < 5 && computer_score < 5){
+        playRound();
+    }
+    if(player_score > computer_score){
+        console.log("Player Wins!");
+    } else {
+        console.log("Computer Wins!");
+    }
+
+    //ask player if they want to play again
+    //if yes, then set player_score and computer_score to 0 again and game()
 }
 
+game();
