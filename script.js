7affd939db552a44c8a3ccb103c3a068b9bb9a81
Scott Park <scott.park.dev@gmail.com>
2022-01-13 16:48:44 -0800

    Attempted to add some JS

--- script.js
+++ script.js
@@ -3,6 +3,20 @@
 let rps = ['rock', 'paper', 'scissors'];
 let player_score = 0, computer_score = 0;
 let player_pick, computer_pick;
+let message_content;
+
+let message = document.getElementsByClassName('message');
+
+let buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', ()=>{
+        if(button.id){
+            console.log(button.id)
+            message.textContent = button.textContent;
+            console.log(message);
+        }
+    });
+});
 
 
 let computerPlay = () =>{
@@ -14,11 +28,17 @@ let playRound = (p, c) => {
     if ((p === "rock" && c === "paper") || (p === "paper" && c === "scissors") || (p === "scissors" && c === "rock")){
         computer_score++;
         console.log("You lose the round! " + p + " loses to " + c);
+        message_content = "You lose the round! " + p + " loses to " + c;
+        message.textContent = message_content;
     } else if ((c === "rock" && p === "paper") || (c === "paper" && p === "scissors") || (c === "scissors" && p === "rock")){
         player_score++;
         console.log("You win the round! " + p + " beats " + c);
+        message_content = "You win the round! " + p + " beats " + c;
+        message.textContent = message_content;
     } else if (p === c){
         console.log("Draw! You both picked " + p);
+        message_content = "Draw! You both picked " + p;
+        message.textContent = message_content;
     }
     console.log("You: " + player_score + " Computer: " + computer_score);
 }
@@ -27,7 +47,7 @@ let game = () =>{
 
     while(player_score < 5 && computer_score < 5){
         computer_pick = computerPlay();
-        player_pick = prompt("Choose: Rock, Paper, or Scissors").toLowerCase();
+        player_pick = "rock";
         playRound(player_pick, computer_pick);
     }
     if(player_score > computer_score){
